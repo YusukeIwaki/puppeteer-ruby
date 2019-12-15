fac9729f8a16b92d51b3b7824b7dcbf86af5a5a5
YusukeIwaki <yusuke-iwaki@example.com>
2019-12-15 12:31:55 +0900

    Update README

---
+++ example.rb
@@ -0,0 +1,8 @@
+require('puppeteer');
+
+Puppeteer.launch(headless: false) do |browser|
+  page = browser.new_pages.first || browser.new_page
+  page.goto("https://github.com/YusukeIwaki")
+  page.screenshot(path: "YusukeIwaki.png")
+  page.close
+end
