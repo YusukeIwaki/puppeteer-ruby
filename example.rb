635acf3812bdf3947c8a94c3ccc2ac19230f1d38
YusukeIwaki <yusuke-iwaki@example.com>
2020-04-09 22:26:45 +0900

    remove old example code

--- example.rb
+++
@@ -1,7 +0,0 @@
-require 'puppeteer';
-
-Puppeteer.launch(headless: false) do |browser|
-  page = browser.pages.first || browser.new_page
-  page.goto("https://github.com/YusukeIwaki")
-  page.screenshot(path: "YusukeIwaki.png")
-end
