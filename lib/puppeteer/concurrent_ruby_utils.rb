54cb191ffaea55a04674924516d3715f8276b40e
YusukeIwaki <iwaki@i3-systems.com>
2020-06-27 01:08:14 +0900

    add comment

--- lib/puppeteer/concurrent_ruby_utils.rb
+++ lib/puppeteer/concurrent_ruby_utils.rb
@@ -1,5 +1,8 @@
 # utility methods for Concurrent::Promises.
 module Puppeteer::ConcurrentRubyUtils
+  # wait for all promises.
+  # REMARK: This method doesn't assure the order of calling.
+  # for example, await_all(async1, async2) calls calls2 -> calls1 often.
   def await_all(*args)
     if args.length == 1 && args[0].is_a?(Enumerable)
       Concurrent::Promises.zip(*(args[0])).value!
@@ -8,6 +11,9 @@ module Puppeteer::ConcurrentRubyUtils
     end
   end
 
+  # wait for first promises.
+  # REMARK: This method doesn't assure the order of calling.
+  # for example, await_all(async1, async2) calls calls2 -> calls1 often.
   def await_any(*args)
     if args.length == 1 && args[0].is_a?(Enumerable)
       Concurrent::Promises.any(*(args[0])).value!
