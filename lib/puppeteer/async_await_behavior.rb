b3b984f65dca07f3bed1984c234559fca5b2b321
YusukeIwaki <iwaki@i3-systems.com>
2020-06-27 22:07:06 +0900

    introduce define_async_method_for method

--- lib/puppeteer/async_await_behavior.rb
+++ lib/puppeteer/async_await_behavior.rb
@@ -1,5 +1,23 @@
 module Puppeteer::AsyncAwaitBehavior
   refine Class do
+    def define_async_method_for(method_name)
+      original_method = instance_method(method_name)
+      async_method_name = "async_#{method_name}"
+
+      if method_defined?(async_method_name) || private_method_defined?(async_method_name)
+        raise ArgumentError.new("#{async_method_name} is already defined")
+      end
+
+      define_method(async_method_name) do |*args|
+        Concurrent::Promises.future do
+          original_method.bind(self).call(*args)
+        rescue => err
+          Logger.new(STDERR).warn(err)
+          raise err
+        end
+      end
+    end
+
     # wrap with Concurrent::Promises.future
     def async(method_name)
       original_method = instance_method(method_name)
--- spec/puppeteer/async_await_behavior_spec.rb
+++ spec/puppeteer/async_await_behavior_spec.rb
@@ -32,4 +32,63 @@ RSpec.describe Puppeteer::AsyncAwaitBehavior do
       expect(res.value!).to eq('my tap!')
     end
   end
+
+  describe 'define_async_method_for' do
+    class Fuga
+      using Puppeteer::AsyncAwaitBehavior
+
+      private def fuga
+        '-> fuga'
+      end
+      define_async_method_for :fuga
+
+      def piyo
+        '-> piyo'
+      end
+      private define_async_method_for :piyo
+    end
+
+    it 'defined async method wrapped with Concurrent::Promises::Future' do
+      instance = Fuga.new
+      expect(instance.async_fuga).to be_a(Concurrent::Promises::Future)
+      expect(instance.async_fuga.value!).to eq('-> fuga')
+    end
+
+    it 'can be used with private' do
+      expect(Fuga.private_method_defined?(:async_piyo)).to eq(true)
+      expect(Fuga.method_defined?(:async_piyo)).to eq(false)
+    end
+
+    it 'raises exception when async method is already defined' do
+      expect {
+        class Ex
+          using Puppeteer::AsyncAwaitBehavior
+
+          private def async_ex
+            "async ex"
+          end
+
+          def ex
+            "ex"
+          end
+          define_async_method_for :ex
+        end
+      }.to raise_error(ArgumentError)
+
+      expect {
+        class Ex
+          using Puppeteer::AsyncAwaitBehavior
+
+          def async_ex
+            "async ex"
+          end
+
+          def ex
+            "ex"
+          end
+          define_async_method_for :ex
+        end
+      }.to raise_error(ArgumentError)
+    end
+  end
 end
