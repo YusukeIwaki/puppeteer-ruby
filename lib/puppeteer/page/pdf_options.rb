01a696da7096a54fbdcc15dadbd23ad295b63a41
YusukeIwaki <iwaki@i3-systems.com>
2021-03-26 14:13:42 +0900

    fix PDF options to work

--- lib/puppeteer/page/pdf_options.rb
+++ lib/puppeteer/page/pdf_options.rb
@@ -52,24 +52,24 @@ class Puppeteer::Page
     end
 
     PAPER_FORMATS = {
-      letter: PaperSize.new(width: 8.5, height: 11),
-      legal: PaperSize.new(width: 8.5, height: 14),
-      tabloid: PaperSize.new(width: 11, height: 17),
-      ledger: PaperSize.new(width: 17, height: 11),
-      a0: PaperSize.new(width: 33.1, height: 46.8),
-      a1: PaperSize.new(width: 23.4, height: 33.1),
-      a2: PaperSize.new(width: 16.54, height: 23.4),
-      a3: PaperSize.new(width: 11.7, height: 16.54),
-      a4: PaperSize.new(width: 8.27, height: 11.7),
-      a5: PaperSize.new(width: 5.83, height: 8.27),
-      a6: PaperSize.new(width: 4.13, height: 5.83),
+      'letter' => PaperSize.new(width: 8.5, height: 11),
+      'legal' => PaperSize.new(width: 8.5, height: 14),
+      'tabloid' => PaperSize.new(width: 11, height: 17),
+      'ledger' => PaperSize.new(width: 17, height: 11),
+      'a0' => PaperSize.new(width: 33.1, height: 46.8),
+      'a1' => PaperSize.new(width: 23.4, height: 33.1),
+      'a2' => PaperSize.new(width: 16.54, height: 23.4),
+      'a3' => PaperSize.new(width: 11.7, height: 16.54),
+      'a4' => PaperSize.new(width: 8.27, height: 11.7),
+      'a5' => PaperSize.new(width: 5.83, height: 8.27),
+      'a6' => PaperSize.new(width: 4.13, height: 5.83),
     }
 
     UNIT_TO_PIXELS = {
-      px: 1,
-      in: 96,
-      cm: 37.8,
-      mm: 3.78,
+      'px' => 1,
+      'in' => 96,
+      'cm' => 37.8,
+      'mm' => 3.78,
     }
 
     # @param parameter [String|Integer|nil]
