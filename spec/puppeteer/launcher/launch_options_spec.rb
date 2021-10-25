5f47c833c24a69bbb0e65ec10e531696734f3312
Anders Carling <lowe@lowe.nu>
2021-10-25 13:15:15 +0200

    Add tests for launch options default values

---
+++ spec/puppeteer/launcher/launch_options_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+RSpec.describe Puppeteer::Launcher::LaunchOptions do
+  describe 'default value' do
+    subject { Puppeteer::Launcher::LaunchOptions.new({}) }
+
+    it 'channel: nil' do
+      expect(subject.channel).to eq(nil)
+    end
+
+    it 'executable_path: nil' do
+      expect(subject.executable_path).to eq(nil)
+    end
+
+    it 'ignore_default_args: false' do
+      expect(subject.ignore_default_args).to eq(false)
+    end
+
+    it 'handle_SIGINT: true' do
+      expect(subject.handle_SIGINT?).to eq(true)
+    end
+
+    it 'handle_SIGTERM: true' do
+      expect(subject.handle_SIGTERM?).to eq(true)
+    end
+
+    it 'handle_SIGHUP: true' do
+      expect(subject.handle_SIGHUP?).to eq(true)
+    end
+
+    it 'timeout: 30000' do
+      expect(subject.timeout).to eq(30000)
+    end
+
+    it 'dumpio: false' do
+      expect(subject.dumpio?).to eq(false)
+    end
+
+    it 'env: ENV' do
+      expect(subject.env).to eq(ENV)
+    end
+
+    it 'pipe: false' do
+      expect(subject.pipe?).to eq(false)
+    end
+  end
+end
