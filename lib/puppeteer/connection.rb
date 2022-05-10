c92cc48712047392732910b20dc7b64999fbac6f
YusukeIwaki <iwaki@i3-systems.com>
2022-05-10 10:29:33 +0900

    Hide annoying logs on firefox

--- lib/puppeteer/connection.rb
+++ lib/puppeteer/connection.rb
@@ -191,6 +191,7 @@ class Puppeteer::Connection
       'Network.responseReceived',
       'Network.responseReceivedExtraInfo',
       'Page.lifecycleEvent',
+      'Target.receivedMessageFromTarget', # only Firefox
     ]
 
     def handle_message(message)
